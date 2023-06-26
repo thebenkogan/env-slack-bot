2e4edef4d68a45967a9552ea2318d9c233745e1e
Ben Kogan <benkogan9@gmail.com>
2023-06-25 21:32:04 -0400

    better formatting

--- src/utils/handlers.ts
+++ src/utils/handlers.ts
@@ -5,15 +5,15 @@ import {
   listEnvs,
   useEnv,
 } from "@/utils/redis";
-import { say, formatSay } from "@/utils/response";
+import { formatSay } from "@/utils/response";
 
 export async function handleCreate(params: URLSearchParams) {
   const env = params.get("text")?.split(" ")[0];
   if (!env) {
-    return say("Please provide a name. For example: /create dev");
+    return formatSay("Please provide a name. For example: `/create dev`.");
   }
   await createEnv(env);
-  return say("Created environment: " + env);
+  return formatSay(`Created environment: \`${env}\``);
 }
 
 export async function handleUse(params: URLSearchParams) {
@@ -21,24 +21,24 @@ export async function handleUse(params: URLSearchParams) {
   const env = splitText[0];
   const description = splitText.slice(1).join(" ") || null;
   if (!env) {
-    return say(
-      "Please provide a name and optionally a description. For example: /use dev super cool feature"
+    return formatSay(
+      "Please provide a name and optionally a description. For example: `/use dev the next big thing`."
     );
   }
 
   const envInfo = await getEnvInfo(env);
   if (!envInfo) {
-    return say(
-      `No environment found with that name. Create it with /create ${env}`
+    return formatSay(
+      `No environment found with that name. Create it with \`/create ${env}\`.`
     );
   }
 
   const { user: existingUser, description: existingDescription } = envInfo;
   if (existingUser) {
     return formatSay(
-      `${env} is currently in use by <@${existingUser}>. ${
+      `\`${env}\` is currently in use by <@${existingUser}>. ${
         existingDescription
-          ? "They are currently working on: " + existingDescription
+          ? `They are currently working on: ${existingDescription}.`
           : ""
       }`
     );
@@ -47,45 +47,47 @@ export async function handleUse(params: URLSearchParams) {
   const userId = params.get("user_id")!;
   // eslint-disable-next-line react-hooks/rules-of-hooks
   await useEnv(env, userId, description);
-  return formatSay(`<@${userId}> is now using ${env}!`);
+  return formatSay(`<@${userId}> is now using \`${env}\`!`);
 }
 
 export async function handleFree(params: URLSearchParams) {
   const env = params.get("text")?.split(" ")[0];
   if (!env) {
-    return say("Please provide a name. For example: /free dev");
+    return formatSay("Please provide a name. For example: `/free dev`.");
   }
 
   const envInfo = await getEnvInfo(env);
   if (!envInfo) {
-    return say(
-      `No environment found with that name. Create it with /create ${env}`
+    return formatSay(
+      `No environment found with that name. Create it with \`/create ${env}\`.`
     );
   }
 
   const { user: existingUser } = envInfo;
   if (!existingUser) {
-    return say(`${env} is not currently in use. Use it with /use ${env}`);
+    return formatSay(
+      `\`${env}\` is not currently in use. Use it with \`/use ${env}\``
+    );
   }
 
   const userId = params.get("user_id")!;
   if (existingUser !== userId) {
     return formatSay(
-      `${env} is currently in use by <@${existingUser}>. Please contact them.`
+      `\`${env}\` is currently in use by <@${existingUser}>. Please contact them.`
     );
   }
 
   await freeEnv(env);
-  return say(`${env} is now free!`);
+  return formatSay(`<!here> \`${env}\` is now free!`);
 }
 
 export async function handleList(params: URLSearchParams) {
   const envs = await listEnvs();
   const messages = envs.map(([env, { user, description }]) => {
     if (!user) {
-      return `${env} is free`;
+      return `\`${env}\` is free.`;
     }
-    return `${env} is in use by <@${user}>. ${
+    return `\`${env}\` is in use by <@${user}>. ${
       description ? `They are currently working on: ${description}.` : ""
     }`;
   });
--- src/utils/response.ts
+++ src/utils/response.ts
@@ -4,13 +4,6 @@ export function error(message: string, status = 500) {
   return NextResponse.json({ error: message }, { status });
 }
 
-export function say(text: string) {
-  return NextResponse.json({
-    response_type: "in_channel",
-    text,
-  });
-}
-
 export function formatSay(text: string) {
   return NextResponse.json({
     response_type: "in_channel",
